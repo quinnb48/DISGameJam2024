3cf070f6c054890412e097befed5aff40cc827a9
izzzzzzzzzzy <dkriz1662@gmail.com>
2024-02-05 13:31:07 +0100

    Added camera rotation with directional inputs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,10 +7,16 @@ public class CameraController : MonoBehaviour
     public Transform player;
     public float offsetX;
     public float offsetY;
+    public Vector2 inputDirection;
+    public float rotationSpeed = 5f;
+
+    private Quaternion targetRotation;
+    private Quaternion initialRotation;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        targetRotation = initialRotation = transform.rotation;
     }
 
     // Update is called once per frame
@@ -20,5 +26,13 @@ public class CameraController : MonoBehaviour
         pos.x = player.position.x + offsetX;
         pos.y = player.position.y + offsetY;
         transform.position = pos;
+    }
+
+    private void FixedUpdate()
+    {
+        inputDirection.x = Input.GetAxis("Horizontal");
+        targetRotation = Quaternion.Euler(0, 0, inputDirection.x * rotationSpeed) * initialRotation;
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
