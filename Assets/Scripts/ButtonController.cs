40439d5ac4661907092babf3fbbec013046ae3a2
Pretrickri <66845017+Pretrickri@users.noreply.github.com>
2024-02-05 13:52:46 +0100

    Jump Mechanics + Button Settings
    ButtonController.cs
    Changed the scale of the Button, and the scale of its collider.

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -5,16 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class ButtonController : MonoBehaviour
 {
-    private Rigidbody2D myRB2D;
+    private Rigidbody2D rb;
 
-    public float velocity = 5f;
-    public float jumpForce = 5f;
+    public float velocity = 12f;
+    public float jumpForce = 0.3f;
     public Vector2 inputDirection;
     
     // Start is called before the first frame update
     void Start()
     {
-        myRB2D = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -26,27 +26,23 @@ public class ButtonController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // Make it move!
         float movementDirection = Input.GetAxis("Horizontal");
-        myRB2D.AddForce(new Vector2(movementDirection, 0f) * velocity * Time.deltaTime);
+        rb.AddForce(new Vector2(movementDirection, 0f) * velocity * Time.deltaTime);
 
+        // Make it jump!
         if (Input.GetButton("Jump") && CanJump())
-        {
-            inputDirection.y = jumpForce;
-        }
+            rb.velocity += new Vector2(0f, jumpForce);
+            //rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        
     }
 
     private bool CanJump()
     {
-        //TODO
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.3f);
 
         if (hit.collider != null)
-        {
-            if (hit.collider.tag == "Platform")
-            {
-                return true;
-            }
-        }
+            {return (hit.collider.CompareTag("Platform"));}
         return false;
     }
 
