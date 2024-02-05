efe56a01d104e4c69267b6808fea0eb9fd23b3a5
izzzzzzzzzzy <dkriz1662@gmail.com>
2024-02-05 13:07:33 +0100

    Added button movement

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonController : MonoBehaviour
 {
     private Rigidbody2D myRB2D;
 
-    public float velocity = 3;
+    public float velocity = 5f;
+    public float jumpForce = 5f;
     public Vector2 inputDirection;
     
     // Start is called before the first frame update
@@ -16,15 +18,40 @@ public class ButtonController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        inputDirection = myRB2D.velocity;
-        inputDirection.x = Input.GetAxis("Horizontal");
-        
+    void Update() { 
+        if (Input.GetKeyDown(KeyCode.R)) {
+            Reset();
+        }
     }
 
     private void FixedUpdate()
     {
-        myRB2D.velocity = inputDirection.normalized * velocity;
+        float movementDirection = Input.GetAxis("Horizontal");
+        myRB2D.AddForce(new Vector2(movementDirection, 0f) * velocity * Time.deltaTime);
+
+        if (Input.GetButton("Jump") && CanJump())
+        {
+            inputDirection.y = jumpForce;
+        }
+    }
+
+    private bool CanJump()
+    {
+        //TODO
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1);
+
+        if (hit.collider != null)
+        {
+            if (hit.collider.tag == "Platform")
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void Reset()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
