92ba84f91a191612927b79a1ec0f36dad15c19c1
Pretrickri <66845017+Pretrickri@users.noreply.github.com>
2024-02-06 13:24:17 +0100

    Made Ant's smoother
    Ants became a platform (you can jump from them) and their mouth hit box was reduced because it was almost impossible to go over them

--- Assets/Scripts/AntMouth.cs
+++ Assets/Scripts/AntMouth.cs
@@ -5,18 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class AntMouth : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     void OnTriggerEnter2D(Collider2D col){
         if(col.gameObject.CompareTag("Player")){
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
